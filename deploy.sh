0519365953ab6183f5aa87ca1ad1bee94907a7b1
nidhi0622 <nidhimehta@microsoft.com>
2022-08-17 10:14:53 -0500

    updating deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -11,10 +11,11 @@ cd $( dirname $0 )
 VERSION=$(cyclecloud project info | grep Version | cut -d: -f2 | cut -d" " -f2)
 DEST_FILE=$(pwd)/blobs/hcheck-linux-$VERSION.tgz
 rm -f $DEST_FILE
-cd ./hcheck/hcheck/bin/Debug/net6.0/
-cp ../../../src/send_log ./linux-x64
-tar czf $DEST_FILE ./linux-x64
+cd ./bin/Debug/net6.0/
+cp ../../../src/send_log /linux-x64
+tar czf hcheck-linux-$VERSION.tgz linux-x64
+cp hcheck-linux-$VERSION.tgz ../../../../../blobs/ 
 cd $( dirname $0 )/
 echo \#!/usr/bin/env bash > ./specs/default/cluster-init/files/version.sh
-echo export HEALTHCHECK_VERSION=$VERSION >> ./specs/default/cluster-init/files/version.sh
-cyclecloud project upload azure-storage
+#echo export HEALTHCHECK_VERSION=$VERSION >> ./specs/default/cluster-init/files/version.sh
+#cyclecloud project upload azure-storage
